0d38eadfee19c3a884f040c00eac8de9f05ca68c
Clint Hill <clint.hill@csaa.com>
2015-12-20 20:17:42 -0700

    Fixing bug with class counts.

--- night-cyc-prototype/src/spec/appClassCountSpec.js
+++ night-cyc-prototype/src/spec/appClassCountSpec.js
@@ -1,5 +1,5 @@
 describe("app class count", function(){
-
+  var SignUp = new window.Parse.Object.extend("SignUp");
   beforeEach(function(){
     spyOn(window.Parse.Promise, "when").and.returnValue({then: function(){}});
     spyOn(window.classInfoQuery, "find");
@@ -10,15 +10,15 @@ describe("app class count", function(){
     var madPhx = window.classInfoQuery.find.calls.first().args[0].success;
     var rpm = window.classInfoQuery.find.calls.mostRecent().args[0].success;
     madPhx([
-      {spinClass: "madison-phoenix", frontRow: true},
-      {spinClass: "madison-phoenix", frontRow: true},
-      {spinClass: "madison-phoenix", frontRow: true},
-      {spinClass: "madison-phoenix", frontRow: true},
-      {spinClass: "madison-phoenix", frontRow: false}
+      new SignUp({spinClass: "madison-phoenix", frontRow: true}),
+      new SignUp({spinClass: "madison-phoenix", frontRow: true}),
+      new SignUp({spinClass: "madison-phoenix", frontRow: true}),
+      new SignUp({spinClass: "madison-phoenix", frontRow: true}),
+      new SignUp({spinClass: "madison-phoenix", frontRow: false})
     ]);
     rpm([
-      {spinClass: "rpm-spin", frontRow: true},
-      {spinClass: "rpm-spin", frontRow: false}
+      new SignUp({spinClass: "rpm-spin", frontRow: true}),
+      new SignUp({spinClass: "rpm-spin", frontRow: false})
     ]);
     expect(window.classCounts["madison-phoenix"].count).toEqual(5);
     expect(window.classCounts["madison-phoenix"].frontRow).toEqual(4);
